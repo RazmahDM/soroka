16cca5f89d05d987c0ba27b64450611c2fac23ef
Student <student@rksi.ru>
2024-04-21 21:37:22 +0300

    htaccess

--- core/wsgi.py
+++ core/wsgi.py
@@ -13,4 +13,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
+activate_this = os.path.expanduser('~/soroka/venv/bin/activate_this.py')
+exec(open(activate_this).read(), {'__file__': activate_this})
+
 application = get_wsgi_application()
